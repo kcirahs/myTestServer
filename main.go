2b587b53a7a4bddb9f5c1e3540f5a7e6f73bff99
kcirahs <32975637+kcirahs@users.noreply.github.com>
2018-02-05 17:27:40 -0500

    served and linked css file

--- main.go
+++ main.go
@@ -6,15 +6,23 @@ import (
 )
 
 var tmpl *template.Template
+var tmplCSS *template.Template
 
 var emails []string
 
 func init() {
 	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+	tmplCSS = template.Must(template.ParseGlob("templates/*.css"))
+}
+
+func styleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/css; charset=utf-8")
+	tmplCSS.Execute(w, nil)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "index.html", nil)
+
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,9 +36,11 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	emails = append(emails, "someguy@gmail.com")
 
-	http.Handle("/", http.HandlerFunc(rootHandler))
-	http.Handle("/about/", http.HandlerFunc(aboutHandler))
-	http.Handle("/contact/", http.HandlerFunc(contactHandler))
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/main.css", styleHandler)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/about/", aboutHandler)
+	http.HandleFunc("/contact/", contactHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
