250f627b97d7a5b70aaf574fb28412b2a959d2a8
kcirahs <32975637+kcirahs@users.noreply.github.com>
2018-02-09 01:06:49 -0500

    modularized flashcard program and added subtraction

--- main.go
+++ main.go
@@ -4,38 +4,18 @@ import (
 	"html/template"
 	"net/http"
 	"log"
-	"math/rand"
-	"strconv"
-	"time"
+	"github.com/kcirahs/myTestServer/utils"
+	"fmt"
 )
 
 var tmpl *template.Template
 
-type addition struct {
-	N1  int
-	N2  int
-	Sum int
-	//AnswerField string
-}
-
-var AddExample addition
-var r1 *rand.Rand
-
 func init() {
 	tmpl = template.Must(template.ParseGlob("templates/*.html"))
-	r1 = rand.New(rand.NewSource(time.Now().UnixNano()))
-}
-
-func generateAdd() (addition) {
-	n1 := r1.Intn(10)
-	n2 := r1.Intn(10)
-	sum := n1 + n2
-	return addition{n1, n2, sum}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "index.html", nil)
-
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,18 +23,20 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func flashCardHandler(w http.ResponseWriter, r *http.Request) {
-
 	if r.Method == http.MethodPost {
-		sum, _:= strconv.Atoi(r.FormValue("sum"))
-		n1, _:= strconv.Atoi(r.FormValue("n1"))
-		n2, _:= strconv.Atoi(r.FormValue("n2"))
-		AddExample = addition{n1, n2, sum}
-		tmpl.ExecuteTemplate(w, "answer.html", AddExample)
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		m := r.Form
+		PreExample := utils.ParsePrevious(m["PreExample"][0])
+		fmt.Println(PreExample)
+		tmpl.ExecuteTemplate(w, "answer.html", PreExample)
 		return
 	}
-	//AddExample.AnswerField = r.FormValue("answer")
-	AddExample = generateAdd()
-	tmpl.ExecuteTemplate(w, "flashCard.html", AddExample)
+	Example := utils.GenerateDiff()
+	//Example := utils.GenerateAdd()
+	tmpl.ExecuteTemplate(w, "flashCard.html", Example)
 }
 
 func main() {
---
+++ utils/flashCard.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+	"strings"
+	"strconv"
+)
+
+const (
+	plus  = "+"
+	minus = "-"
+)
+
+type MathProblem struct {
+	N1       int
+	N2       int
+	Operator string
+	Answer   int
+}
+
+var r1 *rand.Rand
+
+func init() {
+	r1 = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+// GenerateDiff produces the data for a subtraction flash card.
+func GenerateDiff() (MathProblem) {
+	n1 := r1.Intn(19)
+	n2 := r1.Intn(10)
+	if n1 < n2 {
+		n1, n2 = n2, n1
+	}
+	diff := n1 - n2
+	for diff >= 10 {
+		n1 -= 4
+		diff = n1 - n2
+	}
+	return MathProblem{n1, n2, minus, diff}
+}
+
+// GenerateAdd produces the data for an addition flash card.
+func GenerateAdd() (MathProblem) {
+	n1 := r1.Intn(10)
+	n2 := r1.Intn(10)
+	sum := n1 + n2
+	return MathProblem{n1, n2, plus, sum}
+}
+
+// ParsePrevious takes the form data string and converts it back into a MathProblem
+func ParsePrevious(str string) (MathProblem) {
+	str = strings.Trim(str, "{} ")
+	xs := strings.Fields(str)
+	n1, _ := strconv.Atoi(xs[0])
+	n2, _ := strconv.Atoi(xs[1])
+	opr := xs[2]
+	ans, _ := strconv.Atoi(xs[3])
+	return MathProblem{n1, n2, opr, ans}
+}
